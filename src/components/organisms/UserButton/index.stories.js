17bc60cb4ef22c4f1d9dcdf7a153e62a7d153b55
Declan Elcocks <declan@altitudelabs.com>
2017-06-20 11:53:12 +0800

    Update storybook imports

--- src/components/organisms/UserButton/index.stories.js
+++ src/components/organisms/UserButton/index.stories.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { storiesOf, action } from '@kadira/storybook'
+import { storiesOf, action } from '@storybook/react'
 import { UserButton } from 'components'
 
 storiesOf('UserButton', module)
