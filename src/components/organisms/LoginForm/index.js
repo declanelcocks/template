cb217c25f76b7da2ecadb1b27b8e6a3b2c600dda
Declan Elcocks <declan.elcocks@gmail.com>
2017-07-25 22:17:11 +0800

    Add local login form

---
+++ src/components/organisms/LoginForm/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Field } from 'redux-form'
+import styled from 'styled-components'
+
+import { ReduxField, Heading, Button } from 'components'
+
+const Form = styled.form`
+  width: 100%;
+  box-sizing: border-box;
+  padding: 1rem;
+`
+
+const LoginForm = ({ handleSubmit, submitting }) => {
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Field name="email" label="Email" type="email" component={ReduxField} />
+      <Field name="password" label="Password" type="password" component={ReduxField} />
+      <Button type="submit" disabled={submitting}>Sign in</Button>
+    </Form>
+  )
+}
+
+LoginForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  submitting: PropTypes.bool,
+}
+
+export default LoginForm
